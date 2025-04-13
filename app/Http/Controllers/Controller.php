514372e15fa159c335ace279854efb5634a7ef23
jorge aguilera <jorge@incsteps.com>
2025-04-13 21:18:24 +0200

    order swagger servers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,8 @@ use OpenApi\Attributes as OA;
 
 #[
     OA\Info(version: "1.0.0", description: "QA enc/dec api", title: "QA Encription/Decription API"),
-    OA\Server(url: 'http://localhost:8000', description: "local server"),
     OA\Server(url: 'https://qaqatua.com', description: "production server"),
+    OA\Server(url: 'http://localhost:8000', description: "local server"),
     OA\SecurityScheme( securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme: "bearer"),
 ]
 abstract class Controller
